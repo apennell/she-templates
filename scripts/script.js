954d3a8dd87f8a4534268709980dd490cd773c75
Annie Pennell <anniecpennell@gmail.com>
2020-01-12 20:45:38 -0800

    Change colors; make navbar fixed; apply jquery to window scroll

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,24 @@
-// Add your custom JavaScript here
+// Add your custom JavaScript in this file
 
-(function($){
-  $(function(){
+// Everything inside this function will fire once the DOM is ready for JS
+$(function() {
 
-    $('.sidenav').sidenav();
+  // Activate Materialize's sidenav animation with jQuery
+  $('.sidenav').sidenav();
 
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+  window.onscroll = function() {
+    scrollFunction();
+  };
+
+  // Start custom JS here!
+
+}); // end of document ready
+
+
+function scrollFunction() {
+  if ($(document).scrollTop() < 548) {
+    $('.navbar-fixed nav').addClass('no-shadow');
+  } else {
+    $('.navbar-fixed nav').removeClass('no-shadow');
+  }
+}
